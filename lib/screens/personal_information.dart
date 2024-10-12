d9128b80b30b9f2fe4dd6cfe6ca3465840cf7694
ronfrancisco24 <aaronmatt2004@gmail.com>
2024-10-12 17:42:10 +0800

    added scrollbar to necessary pages
    scrollbar now added to home, profile, and settings page

--- lib/screens/personal_information.dart
+++ lib/screens/personal_information.dart
@@ -18,122 +18,123 @@ class PersonalInfoPage extends StatelessWidget {
           title: const Text('Profile Information'),
         ),
         bottomNavigationBar: kBottomAppBar(context),
-          body: SafeArea(
-            child: Center(
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          blurRadius: 10,
-                          spreadRadius: 2,
-                          offset: Offset(0,5),
-                        )
-                      ]
-                    ),
-                      child: kCircleAvatar),
-                  Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(top: 30),
-                        width: 300,
-                        child: TextField(
-                          // Username field
-                          decoration: InputDecoration(
-                            border:UnderlineInputBorder(
-                              borderSide: BorderSide(width: 5),
-                            ),
-                            helperText: 'Username',
+          body: ListView(
+            children: [SafeArea(
+              child: Center(
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            blurRadius: 10,
+                            spreadRadius: 2,
+                            offset: Offset(0,5),
+                          )
+                        ]
+                      ),
+                        child: kCircleAvatar),
+                    Container(
+                      margin: EdgeInsets.only(top: 30),
+                      width: 300,
+                      child: TextField(
+                        // Username field
+                        decoration: InputDecoration(
+                          border:UnderlineInputBorder(
+                            borderSide: BorderSide(width: 5),
                           ),
+                          helperText: 'Username',
                         ),
-                      )),
-                  Expanded(child: Container(
-                    margin: EdgeInsets.only(top: 30),
-                    width: 300,
-                    child: TextField(
-                      // Username field
-                      decoration: InputDecoration(
-                        border:UnderlineInputBorder(
-                          borderSide: BorderSide(width: 5),
-                        ),
-                        helperText: 'Your Name',
                       ),
                     ),
-                  )),
-                  Expanded(child: Container(
-                    margin: EdgeInsets.only(top: 30),
-                    width: 300,
-                    child: TextField(
-                      // Username field
-                      decoration: InputDecoration(
-                        border:UnderlineInputBorder(
-                          borderSide: BorderSide(width: 5),
+                    Container(
+                      margin: EdgeInsets.only(top: 30),
+                      width: 300,
+                      child: TextField(
+                        // Username field
+                        decoration: InputDecoration(
+                          border:UnderlineInputBorder(
+                            borderSide: BorderSide(width: 5),
+                          ),
+                          helperText: 'Your Name',
                         ),
-                        helperText: 'Email',
                       ),
                     ),
-                  )),
-                  Expanded(child: Container(
-                    margin: EdgeInsets.only(top: 30),
-                    width: 300,
-                    child: TextField(
-                      // Username field
-                      decoration: InputDecoration(
-                        border:UnderlineInputBorder(
-                          borderSide: BorderSide(width: 5),
+                    Container(
+                      margin: EdgeInsets.only(top: 30),
+                      width: 300,
+                      child: TextField(
+                        // Username field
+                        decoration: InputDecoration(
+                          border:UnderlineInputBorder(
+                            borderSide: BorderSide(width: 5),
+                          ),
+                          helperText: 'Email',
                         ),
-                        helperText: 'Your Birthday',
                       ),
                     ),
-                  )),
-                  Expanded(child: Container(
-                    margin: EdgeInsets.only(top: 30),
-                    width: 300,
-                    child: TextField(
-                      // Username field
-                      decoration: InputDecoration(
-                        border:UnderlineInputBorder(
-                          borderSide: BorderSide(width: 5),
+                    Container(
+                      margin: EdgeInsets.only(top: 30),
+                      width: 300,
+                      child: TextField(
+                        // Username field
+                        decoration: InputDecoration(
+                          border:UnderlineInputBorder(
+                            borderSide: BorderSide(width: 5),
+                          ),
+                          helperText: 'Your Birthday',
                         ),
-                        helperText: 'Password',
                       ),
                     ),
-                  )),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Container(
-                    width: 300,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      gradient: kLinearGradient,
-                      borderRadius: BorderRadius.circular(20.0),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 10,
-                          blurStyle: BlurStyle.normal,
+                    Container(
+                      margin: EdgeInsets.only(top: 30),
+                      width: 300,
+                      child: TextField(
+                        // Username field
+                        decoration: InputDecoration(
+                          border:UnderlineInputBorder(
+                            borderSide: BorderSide(width: 5),
+                          ),
+                          helperText: 'Password',
                         ),
-                      ],
+                      ),
                     ),
-                    child: Center(
-                        child: Text('Save Changes',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontFamily: 'FontsFree',
-                          fontWeight: FontWeight.normal,
-                        ),)
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      width: 300,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        gradient: kLinearGradient,
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 10,
+                            blurStyle: BlurStyle.normal,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                          child: Text('Save Changes',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontFamily: 'FontsFree',
+                            fontWeight: FontWeight.normal,
+                          ),)
+                      )
+                    ),
+                    SizedBox(
+                      height: 50,
                     )
-                  ),
-                  SizedBox(
-                    height: 50,
-                  )
-                ],
-              ),
-            )
-          )
+                  ],
+                ),
+              )
+            ),
+          ],)
       )
     );
   }
